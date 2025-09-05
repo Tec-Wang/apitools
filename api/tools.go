3b317ae0f9fb8dadbce166c7bacd22198db516e4
wangzheng <wangzheng@powerlaw.ai>
2025-09-05 15:37:11 +0800

    struct average time

--- api/tools.go
+++ api/tools.go
@@ -3,12 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 
 	"apitools/api/internal/config"
 	"apitools/api/internal/handler"
 	"apitools/api/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
 )
 
@@ -23,9 +25,19 @@ func main() {
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
+	server.Use(requestDetailMiddleware)
+
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
+}
+
+func requestDetailMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logx.Infof("request detail: %s %s", r.Method, r.URL.Path)
+		logx.Infof("request body: %s", r.Body)
+		next(w, r)
+	}
 }
